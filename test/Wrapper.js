75cd92e5b44a02aa630c9ca1380f3434e3a02b62
Ron Gierlach <rongierlach@gmail.com>
2017-04-09 12:08:08 -0500

    fix path to store in Wrapper

--- test/Wrapper.js
+++ test/Wrapper.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import store, { immutable as immutableStore } from '../store'
+import store, { immutable as immutableStore } from './store'
 
 const makeWrapper = store => props =>
   <Provider store={store}>
