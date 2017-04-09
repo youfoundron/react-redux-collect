f945449812342ae484a045c46cf066b1961d17a6
Ron Gierlach <rongierlach@gmail.com>
2017-04-09 12:20:24 -0500

    init src entry

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import * as util from './util'
+import createCollectFactory from './createCollectFactory'
+
+const createCollect = createCollectFactory(util)
+
+export default createCollect()
+export { createCollect }
