392956d57b7ff31009d9bdd7d845ad4d700d5619
Ron Gierlach <rongierlach@gmail.com>
2017-04-11 15:13:17 -0500

    util comments and improvements

--- src/util.js
+++ src/util.js
@@ -3,48 +3,55 @@ import R from 'ramda'
 // take the first element of an array
 const first = R.nth(0)
 
+// check if path is an array
+const isArray = path => Array.isArray(path)
+
+// determine if the given propName is the spread symbol
+const isSpread = R.equals('...')
+
 // determine if the given path is indicating the wildcard
 const isWildcard = R.ifElse(
-  Array.isArray,
+  isArray,
   R.compose(R.equals('*'), first),
   R.equals('*')
 )
 
 // determine if the given path is indicating an action
 const isActionPath = R.ifElse(
-  Array.isArray,
-  R.compose(R.equals('actions', first)),
+  isArray,
+  R.compose(R.equals('actions'), first),
   R.always(false)
 )
 
-// determine if the given propName is the spread symbol
-const isSpread = R.equals('...')
-
 // generate a ramda lens given a path
 const getLensFromPath = R.ifElse(
-  Array.isArray,
+  isArray,
   R.lensPath,
   R.lensProp
 )
 
 // get a state value given a path and state
-const getValueFromPath = R.useWith(R.view, [getLensFromPath, R.identity])
+const getValueFromPath = R.useWith(
+  R.view,
+  [getLensFromPath, R.identity]
+)
 
 // get a propName given a path
 const getPropNameFromPath = R.ifElse(
-  Array.isArray,
+  isArray,
   R.last,
   R.identity
 )
 
 // given a path, return a function to retrieve an actionCreator
-const getActionCreatorFactory = R.path(R.slice(1))
+const getActionCreatorFactory = R.compose(R.path, R.slice(1))
 
 export {
-  getValueFromPath,
-  getPropNameFromPath,
-  getActionCreatorFactory,
+  isArray,
+  isSpread,
   isWildcard,
   isActionPath,
-  isSpread
+  getValueFromPath,
+  getPropNameFromPath,
+  getActionCreatorFactory
 }
