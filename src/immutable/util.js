820f16f3b239730383b3c76e905e6c95e784bc93
Ron Gierlach <rongierlach@gmail.com>
2017-04-21 00:55:56 -0500

    immutable util

--- src/immutable/util.js
+++ src/immutable/util.js
@@ -1,3 +1,10 @@
-export {
+import R from 'ramda'
+import { isArray } from '../util'
 
-}
+const getValueFromPath = R.ifElse(
+  isArray,
+  (path, state) => state.getIn(path),
+  (path, state) => state.get(path)
+)
+
+export { getValueFromPath }
