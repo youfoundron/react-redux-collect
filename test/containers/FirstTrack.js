bc5f6bad4f78dce005a363b9cf473ab42acdafd3
Ron Gierlach <rongierlach@gmail.com>
2017-04-23 13:32:55 -0500

    write a test container

---
+++ test/containers/FirstTrack.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import collect from '../../src'
+import immutableCollect from '../../src/immutable'
+import TrackDetail from '../components/TrackDetail'
+import {
+  selectFirstTrack,
+  immutableSelectFirstTrackselect
+} from '../containers/selectors'
+
+const FirstTrack = collect(
+  [selectFirstTrack, '...']
+)(TrackDetail)
+
+const ImmutableFirstTrack = immutableCollect(
+  [immutableSelectFirstTrack, '...']
+)(TrackDetail)
+
+export default FirstTrack
+export const immutable = ImmutableFirstTrack
